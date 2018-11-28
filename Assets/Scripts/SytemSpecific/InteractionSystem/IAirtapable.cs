ef1713fa3c7a8e8cd849f40f1018daa063a0bbc8
BNJMO <dj_bnjmo@hotmail.fr>
2018-11-28 14:44:02 +0100

    Last project update

---
+++ Assets/Scripts/SytemSpecific/InteractionSystem/IAirtapable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public interface IAirtapable
+{
+    void OnTap(InteractionSourceKind source, int tapCount, Ray headRay);
+    void OnHoldStart(InteractionSourceKind source, Ray headRay);
+    void OnHoldFinish(InteractionSourceKind source, Ray headRay);
+}
